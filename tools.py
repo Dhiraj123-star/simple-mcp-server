cbd7c1015c1ded6e845e463de10f327add778a24
Dhiraj123-star <gdhiraj499@gmail.com>
2026-06-27 20:34:34 +0530

    feat: add core helper functions for math and greet operations

---
+++ tools.py
@@ -0,0 +1,11 @@
+def add(a:int, b: int) -> int:
+    """ Add two numbers """
+    return a+b
+
+def greet(name:str) ->str:
+    """Greet someone by name"""
+    return f"Hello {name}! Welcome to MCP!!"
+
+def multiply(a:int,b:int)-> int:
+    """Multiply two numbers"""
+    return a*b
